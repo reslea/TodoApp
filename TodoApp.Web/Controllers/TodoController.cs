4419de8bb046a0670ea5329c708d98e61f692a8e
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2020-05-10 12:54:19 +0300

    update todo edit action

--- TodoApp.Web/Controllers/TodoController.cs
+++ TodoApp.Web/Controllers/TodoController.cs
@@ -65,5 +65,28 @@ namespace TodoApp.Web.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(TodoModel editModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.LogInformation("invalid form was submitted");
+                return View("Error", new ErrorViewModel());
+            }
+
+            var dbTodo = _mapper.Map<Todo>(editModel);
+            try
+            {
+                await _todoService.Edit(dbTodo);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
